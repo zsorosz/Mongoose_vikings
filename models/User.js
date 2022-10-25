eaffa2cf8a31a7a1b999d5ee549baa0c36f7fc56
Joshua George <horheyinc8@gmail.com>
2022-10-25 22:34:28 +0200

    updated 1

--- models/User.js
+++ models/User.js
@@ -1,9 +1,22 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  //Iteration 1:
-  //Define the Schema (shape) of the information for your collection
+  //first name is a string and is required
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: String,
+  age: Number,
+  //family members is an array of strings
+  familyMembers: [
+    {
+      type: String,
+    },
+  ],
 });
 
+//create a model with two arguments ====> (the name of the collection, schema or shape of data)
 const UserModel = model("user", userSchema);
+//always make sure you export you model to use in other files
 module.exports = UserModel;
