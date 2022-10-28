286e6f5331506a68ddf6863c441f6582048282d7
Joshua George <horheyinc8@gmail.com>
2022-10-28 10:32:48 +0200

    finish all iterations

--- db/index.js
+++ db/index.js
@@ -1,12 +1,15 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
-
+const VikingModel = require("../models/User");
 mongoose
   .connect("mongodb://127.0.0.1/valhalla", { family: 4 })
   .then(() => {
     console.log(`Connected to Mongo! Welcome to Valhalla!  🪓🛡️☠️`);
+    // remove all the vikings from the DB to start each save
+    return VikingModel.deleteMany();
   })
+  .then(() => console.log("Valhalla is empty"))
   .catch((err) => {
     console.error("Error connecting to DB", err);
   });
--- models/User.js
+++ models/User.js
@@ -1,7 +1,21 @@
 // require mongoose and destructor Schema and model from it
-
+const { Schema, model } = require("mongoose");
 //Create a viking schema here
 //<================Schema=====================>
+const vikingSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  familyName: {
+    type: String,
+    required: true,
+  },
+  strength: Number,
+  weapon: [String],
+});
 
 //create a model with two arguments ====> (the name of the collection, schema or shape of data)
+const VikingModel = model("fallenViking", vikingSchema);
 //always make sure you export you model to use in other files
+module.exports = VikingModel;
